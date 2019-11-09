69f0aaf9520c6e589d7715bad2d550d5e5f98d1e
Abhinay <abhinaybhatt2121999@gmail.com>
2019-11-09 16:47:29 +0530

    quizmania files

---
+++ noBack.js
@@ -0,0 +1,36 @@
+<script>
+      (function (global) {
+
+if(typeof (global) === "undefined")
+{
+  throw new Error("window is undefined");
+}
+
+  var _hash = "!";
+  var noBackPlease = function () {
+      global.location.href += "#";
+
+  // making sure we have the fruit available for juice....
+  // 50 milliseconds for just once do not cost much (^__^)
+      global.setTimeout(function () {
+          global.location.href += "!";
+      }, 50);
+  };  
+
+// Earlier we had setInerval here....
+  global.onhashchange = function () {
+      if (global.location.hash !== _hash) {
+          global.location.hash = _hash;
+      }
+  };
+
+  global.onload = function () {
+      
+  noBackPlease();
+
+ 
+  };
+noBackPlease();
+})(window);
+    
+    </script>
